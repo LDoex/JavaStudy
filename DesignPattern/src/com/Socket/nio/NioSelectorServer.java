f2b0b4a9afd7a0bbe5ec4f094db78a163dc2e3f7
oyyk <745691616@qq.com>
2021-09-30 17:12:45 +0800

    带selector的nio实现

--- DesignPattern/src/com/Socket/nio/NioSelectorServer.java
+++ DesignPattern/src/com/Socket/nio/NioSelectorServer.java
@@ -2,9 +2,13 @@ package com.Socket.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NioSelectorServer {
     public static void main(String[] args) throws IOException {
@@ -23,6 +27,37 @@ public class NioSelectorServer {
             //阻塞等待需要处理的事件发生
             selector.select();
 
+            //获取selector中注册的全部事件的SelectionKey实例
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+            //遍历SelectionKey对事件进行处理
+            while(iterator.hasNext()){
+                SelectionKey key = iterator.next();
+                //如果是OP_ACCEPT事件，则进行连接获取和事件注册
+                if(key.isAcceptable()){
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = server.accept();
+                    socketChannel.configureBlocking(false);
+                    //注册读事件
+                    SelectionKey selKey = socketChannel.register(selector, SelectionKey.OP_READ);
+                    System.out.println("连接成功");
+                } else if(key.isReadable()){ //如果是OP_READ事件，则进行读取和打印
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(128);
+                    int len = socketChannel.read(byteBuffer);
+                    //如果有数据就打印
+                    if(len>0){
+                        System.out.println("接收到消息"+new String(byteBuffer.array()));
+                    } else if(len==-1){
+                        System.out.println("客户端断开连接");
+                        socketChannel.close();
+                    }
+                }
+                //从事件集合里删除本次处理的可以，防止下次select重复处理
+                iterator.remove();
+
+            }
         }
     }
 }
